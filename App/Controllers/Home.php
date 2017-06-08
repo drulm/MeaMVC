b48edc87a56f516f2992bcbbf6c4b4f8f83639c3
Darrell Ulm <darrellulm@gmail.com>
2017-06-08 18:18:54 -0400

    Add twig templating

--- App/Controllers/Home.php
+++ App/Controllers/Home.php
@@ -40,7 +40,13 @@ class Home extends \Core\Controller
      */
     public function indexAction()
     {
+        /*
         View::render('Home/index.php', [
+            'name'    => 'Dave',
+            'colours' => ['red', 'green', 'blue']
+        ]);
+        */
+        View::renderTemplate('Home/index.html', [
             'name'    => 'Dave',
             'colours' => ['red', 'green', 'blue']
         ]);
---
+++ App/Views/Home/index.html
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Home</title>
+</head>
+<body>
+    <h1>Welcome</h1>
+    <p>Hello from a Twig template, {{ name }}!</p>
+
+    <ul>
+        {% for colour in colours %}
+            <li>{{ colour }}</li>
+        {% endfor %}
+    </ul>
+</body>
+</html>
