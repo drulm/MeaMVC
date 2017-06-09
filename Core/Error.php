25acb7de211df1ade2a96e7fd8af42a1a0276516
Darrell Ulm <darrellulm@gmail.com>
2017-06-09 17:00:18 -0400

    template error pages

--- Core/Error.php
+++ Core/Error.php
@@ -60,11 +60,14 @@ class Error
 
             error_log($message);
             //echo "<h1>An error occurred</h1>";
+            /*
             if ($code == 404) {
                 echo "<h1>Page not found</h1>";
             } else {
                 echo "<h1>An error occurred</h1>";
             }
+            */
+            View::renderTemplate("$code.html");
         }
     }
 }
